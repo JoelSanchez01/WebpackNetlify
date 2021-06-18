6a1af9b63ac7e986f76e0059bfc1eb9eb7d885a2
Joel Sanchez <joeleduardosanchezherrera@gmail.com>
2021-06-18 17:08:24 -0500

    Se agrego la clase de css y pre procesadores, y copiar archivos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Template from './templates/Template.js';
 import './styles/main.css'
-console.log('hola');
+import './styles/styles.scss'
 
 (async function App() {
   const main = null || document.getElementById('main');
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -22,9 +22,10 @@ module.exports = {
                 }
         },
         {
-            test: /\.css$/i,
+            test: /\.css|.s[ac]ss$/i,
             use: [MiniCssExtractPlugin.loader, 
-            'css-loader'
+            'css-loader',
+            'sass-loader'
         ],
         }
     ]
@@ -36,6 +37,14 @@ plugins: [
   filename: './index.html' // default index.html
     }),
     new MiniCssExtractPlugin(),
+    new CopyPlugin({
+        patterns: [
+        {
+            from:path.resolve(__dirname, "src", "assets/images"),
+            to: "assets/images"
+        }
+        ]
+    })
   ],
 
 }
