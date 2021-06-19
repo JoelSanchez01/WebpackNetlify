0ed897ce3ddb6608914d649a5fc40a13b2487c2d
Joel Sanchez <joeleduardosanchezherrera@gmail.com>
2021-06-19 02:18:17 -0500

    se agrego todo hasta la clase 12

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     resolve: {
         extensions: ['.js'],
@@ -27,6 +30,24 @@ module.exports = {
             'css-loader',
             'sass-loader'
         ],
+        },
+        {
+            test: /\.png/,
+            type: 'asset/resource'   
+        },
+        {
+            test: /\.(woff|woff2)$/,
+            use:{
+                loader:'url-loader',
+                options: {
+                    limit:10000,
+                    mimetype: "application/font-wolf",
+                    name: "[name].[contenthash].[ext]",
+                    outputPath: "./assets/fonts/",
+                    publicPath: "./assets/fonts/",
+                    esModule: false,
+                    },
+                }
         }
     ]
 },
@@ -36,7 +57,9 @@ plugins: [
       template:"/public/index.html",
   filename: './index.html' // default index.html
     }),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+        filename: 'assets/[name].[contenthash].css'
+      }),
     new CopyPlugin({
         patterns: [
         {
@@ -44,7 +67,14 @@ plugins: [
             to: "assets/images"
         }
         ]
-    })
+    }),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ]
+  }
 
 }
